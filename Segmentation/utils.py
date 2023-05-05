eb6adf15440f361b59d3c25ef710f6b59e23ba31
Marlon Rodrigues Garcia <marlonrg@gmail.com>
2023-05-05 08:47:16 -0300

    commenting better utils.py

--- Segmentation/utils.py
+++ Segmentation/utils.py
@@ -9,10 +9,11 @@ import random
 from torchmetrics import Dice
 
 
-# The next functions are to define classes of functional transforms, used to 
-# apply a deterministic transformation in more than one entry, e.g. a random
-# rotation, that can be equaly applied for an image and its label (it neets to
-# be deterministic fot the label, the same of the image).
+# The next functions are functional transforms, used to apply functions in a way
+# controled by the user. So we can apply, for example, in the data image and in
+# the label image (so it is called deterministic, because we can determine the
+# same transformation to be applied in more then one image). This is the unique
+# way to apply the same transformation to more then one different image in torch
 
 class ToTensor(object):
     '''Function to transform a ndarray in a tensor
@@ -83,6 +84,7 @@ class CenterCrop(object):
             images[image] = tf.center_crop(images[image], self.size)
         
         return images
+
 
 class Resize(object):
     '''Function to resize one or multiple images
@@ -158,6 +160,7 @@ class Normalize(object):
         
         return images
 
+
 class Affine(object):
     '''Affining images
     
@@ -183,17 +186,22 @@ class Affine(object):
                                          shear=shear)
         return images
 
-## Util functions
+
+#%% Util Functions to be Used During Training or Testing
+
+# saving checkpoints
 def save_checkpoint(state, filename='my_checkpoint.pth.tar'):
     print('- Saving Checkpoint...')
     torch.save(state, filename)
 
+# loading checkpoints
 def load_checkpoint(checkpoint, model, optimizer=None):
     print('- Loading Checkpoint...')
     model.load_state_dict(checkpoint['state_dict'])
     if optimizer:
         optimizer.load_state_dict(checkpoint['optimizer'])
 
+# getting loaders given directories and other informations
 def get_loaders(train_image_dir,
                 valid_percent,
                 test_percent,
@@ -206,34 +214,35 @@ def get_loaders(train_image_dir,
                 clip_valid=1.0,
                 clip_train=1.0):
     
+    # first, defining transformations to be applied in the train images to be loaded
     transform_train_0 = Compose([ToTensor(n=1),
                                  Resize(size=[image_height, image_width]),
                                  FlipVertical(p=0.5),
                                  FlipHorizontal(p=0.5),
                                  # Mean and std, obtained from the dataset
-                                  Normalize(n=1, mean=[0.52096, 0.51698, 0.545980],
-                                            std=[0.10380, 0.11190, 0.118877])]
+                                 Normalize(n=1, mean=[0.52096, 0.51698, 0.545980],
+                                           std=[0.10380, 0.11190, 0.118877])]
                                 )
-    
+    # defining the same, but for validation and testing images (can be different)
     transform_valid_0 = Compose([ToTensor(n=1),
                                  Resize(size=[image_height, image_width]),
                                  FlipVertical(p=0.5),
                                  FlipHorizontal(p=0.5),
                                  # Mean and std, obtained from the dataset
-                                  Normalize(n=1, mean=[0.52096, 0.51698, 0.545980],
-                                                  std=[0.10380, 0.11190, 0.118877])]
+                                 Normalize(n=1, mean=[0.52096, 0.51698, 0.545980],
+                                           std=[0.10380, 0.11190, 0.118877])]
                                 )
     
-    # Number of transformations per dataset
+    # second, defining the number of transformations per directory in 'train_image_dir'
     transformations_per_dataset = [1, 1, 1, 1, 1, 1, 1]
     
-    # Firsta part of whole dataset, only with images in 'train_image_dir[0]'
+    # third, reading the dataset in a as a 'torch.utils.data.Dataset' instance.
+    # it is only for images in 'train_image_dir[0]', next we accounts for the rest
     train_dataset = RaabinDataset(image_dir=train_image_dir[0],
                                   transform=transform_train_0)
     
-    # Concatenate in 'train_image_dir', that are different datasets to be added
-    # the other for in 'transf.._per_dataset' will be to data augmentation (it
-    # is not different datasets, is to really augment the data)
+    # concatenate the other directories in 'train_image_dir[:]' in a larger
+    # 'torhc.utils.data.Dataset'. after we concatenate more to augment the data
     for n in range(1, len(train_image_dir)):
         dataset_train_temp = RaabinDataset(image_dir=train_image_dir[n],
                                            transform=transform_train_0)
@@ -241,7 +250,7 @@ def get_loaders(train_image_dir,
         train_dataset = torch.utils.data.ConcatDataset([train_dataset,
                                                         dataset_train_temp])
     
-    # Using part of the training data as test datset
+    # using part of the training data as test dataset
     test_dataset_size = int(test_percent*len(train_dataset))
     rest_size = int((1-test_percent)*len(train_dataset))
     if test_dataset_size+rest_size != len(train_dataset):
@@ -250,12 +259,12 @@ def get_loaders(train_image_dir,
                                      generator=(torch.Generator().manual_seed(40)))
     
     
-    # Defining the validation dataset, using part of 'train_dataset', or using
-    # a specific dataset for validation, if 'val_image_dir' is not 'None'
+    # defining the validation dataset, using part of the 'train_dataset', or
+    # using a specific dataset for validation, if 'val_image_dir' is not 'None'
     if not val_image_dir:
         valid_dataset_size = int(valid_percent*len(train_dataset))
         train_dataset_size = int((1-valid_percent)*len(train_dataset))
-        # Adding one to train_dataset_size if 'int' operation removed it
+        # adding one to train_dataset_size if 'int' operation removed it
         if valid_dataset_size+train_dataset_size != len(train_dataset):
             train_dataset_size += 1
         (train_dataset, valid_dataset) = random_split(train_dataset,
@@ -270,9 +279,10 @@ def get_loaders(train_image_dir,
             valid_dataset = torch.utils.data.ConcatDataset([valid_dataset,
                                                             dataset_val_temp])
     
-    # Following ther rest of augmented data
+    # concatenating the augmented data, in case 'transf..._per_dataset' > 1
     for n in range(0,len(train_image_dir)):
         for m in range(1, transformations_per_dataset[n]):
+            # first we specify the transformation (depending on the 'm' value)
             if m < 2:
                 transformation = Compose([ToTensor(n=1),
                                           Rotate(limit=[(m-1)*72,m*72], p=1.0),
@@ -295,12 +305,12 @@ def get_loaders(train_image_dir,
                                           Normalize(n=1, mean=[0.52096, 0.51698, 0.545980],
                                                     std=[0.10380, 0.11190, 0.118877])]
                                           )
-            
+            # then we apply this transformation to read the dataset as 'torch.utils.data.Dataset'
             dataset_train_temp = RaabinDataset(image_dir=train_image_dir[n],
                                                transform=transformation)
             train_dataset = torch.utils.data.ConcatDataset([train_dataset, dataset_train_temp])
     
-    # Splitting the dataset, if it is too large, to deminish if 'clip_valid'<1
+    # splitting the dataset, to deminish if 'clip_valid'<1 for fast testing
     if clip_train < 1:
         print('- Splitting Training Dataset ',clip_train*100,'%')
         train_mini = int(clip_train*len(train_dataset))
@@ -320,7 +330,7 @@ def get_loaders(train_image_dir,
         (test_dataset, _) = random_split(test_dataset, [valid_mini, temp_mini],
                                          generator=torch.Generator().manual_seed(50))
     
-    # Obtaining dataloader from dataset
+    # Obtaining dataloader from the datasets defined above
     train_loader = DataLoader(train_dataset, batch_size=batch_size,
                               num_workers=num_workers,
                               pin_memory=pin_memory, shuffle=True)
@@ -333,6 +343,7 @@ def get_loaders(train_image_dir,
     
     return train_loader, test_loader, valid_loader
 
+# functino to check accuracy
 def check_accuracy(loader, model, loss_fn, device='cuda' if torch.cuda.is_available() else 'cpu', **kwargs):
     num_correct = 0
     num_pixels = 0
@@ -342,7 +353,7 @@ def check_accuracy(loader, model, loss_fn, device='cuda' if torch.cuda.is_availa
     title = kwargs.get('title')
     if title==None: title = ''
     else: title = title+': '
-    
+    # using tqdm.tqdm to show a progress bar
     loop = tqdm(loader, desc=title+'Check acc')
     
     with torch.no_grad():
@@ -379,7 +390,7 @@ def check_accuracy(loader, model, loss_fn, device='cuda' if torch.cuda.is_availa
     model.train()
     return 100*num_correct_item/num_pixels, loss_item, 100*dice_score_item/len_loader
 
-
+# saving images (only if the output are images)
 def save_predictions_as_imgs(loader, model, folder='saved_images',
                              device='cuda' if torch.cuda.is_available() else 'cpu',
                              **kwargs):
